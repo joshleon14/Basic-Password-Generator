53c8f12a7608027631aedac8179cc2f7317da562
Joshua Leon <joshjleon14@gmail.com>
2018-11-03 12:21:52 -0500

    inital commit of password generator

---
+++ passwordGen.py
@@ -0,0 +1,23 @@
+# password Generator
+import random
+
+class passwordCreator:
+    password = ""
+    chars = "abcdefghijklmnopqrstuvwxyz.,/\|[]{}""::`~<>!@#$%^&*()-=_+1234567890"
+
+    def numberGenerator(self):
+        return random.randint(1,50)
+
+    def passwordGenerator(self):
+        length = self.numberGenerator()
+        for i in range(length):
+            self.password += random.choice(self.chars)
+        self.showPassword(length)
+
+    def showPassword(self, length):
+        print("The predefined Length is: {}".format(length))
+        print("This is your new password: ")
+        print(self.password)
+
+
+passwordCreator().passwordGenerator()
